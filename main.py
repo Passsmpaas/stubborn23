4b3c4a78d22bd6d5797fb204d1d06196522fbeeb
Shivamparashar23 <146502436+Shivamparashar23@users.noreply.github.com>
2025-08-05 12:46:29 +0530

    Update main.py

--- main.py
+++ main.py
@@ -522,7 +522,7 @@ async def txt_handler(bot: Client, m: Message):
 
             elif "https://cpvod.testbook.com/" in url:
                 url = url.replace("https://cpvod.testbook.com/", "https://media-cdn.classplusapp.com/drm/")
-                api_url = f"https://cpapi-rjbs-1l0p.onrender.com/extract_keys?url={url}@bots_updatee&user_id={USER_ID}"
+                api_url = f"https://cpapi-rjbs.onrender.com/extract_keys?url={url}@bots_updatee&user_id={USER_ID}"
                 mpd, keys = helper.get_mps_and_keys(api_url)
                 url = mpd
                 keys_string = " ".join([f"--key {key}" for key in keys])
